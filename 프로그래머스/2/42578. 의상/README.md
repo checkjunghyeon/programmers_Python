d18d8929a684833ef957914a7783eb5c929f9dd7
최정현 <83526938+checkjunghyeon@users.noreply.github.com>
2024-10-18 15:14:26 +0900

    [level 2] Title: 의상, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,6 +1,6 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 18일 15:12:04
+2024년 10월 18일 15:14:26
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,20 +1,13 @@
 import math
-from collections import Counter
 
 def solution(clothes):
     dic = {}
     for c in clothes:
-        c_name = c[0]
-        c_type = c[1]
-
-        if c_type in dic:
-            dic[c_type].append(c_name)
+        if c[1] in dic:
+            dic[c[1]].append(c[0])
         else:
-            dic[c_type] = [c_name]
-    # if len(dic.keys()) < 2: 
-    #     sum =-1
-    # else:
-    #     sum = 0
+            dic[c[1]] = [c[0]]
+
     sum = 1
     for v in dic.values():
         l = len(v)
