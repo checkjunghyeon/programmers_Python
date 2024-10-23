c520cfb7f15369cf40a75857d7a3d027a5a2660d
최정현 <83526938+checkjunghyeon@users.noreply.github.com>
2024-10-24 00:02:42 +0900

    [level 2] Title: 기능개발, Time: 0.08 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,20 +1,17 @@
-from collections import deque
-import math 
-
+import math
 def solution(progresses, speeds):
     answer = []
 
-    remainedDays = list(map(lambda x, y: math.ceil((100 - x)/y), progresses, speeds))
+    daysToComplete = list(map(lambda x, y: math.ceil((100 - x)/y), progresses, speeds))
     
-    _max = remainedDays[0]
+    currentMaxDay = daysToComplete[0]
     n = 0
-    for i, d in enumerate(remainedDays):
+    for i in range(len(daysToComplete)):
         n += 1
-        if i == (len(remainedDays)-1):
-            answer.append(n)           
-        elif _max < remainedDays[i+1]:
+        if i == len(daysToComplete)-1 or currentMaxDay < daysToComplete[i+1]:
             answer.append(n)
             n = 0
-            _max = remainedDays[i+1]
+            if i != len(daysToComplete) - 1:
+                currentMaxDay = daysToComplete[i+1]
         
     return answer
