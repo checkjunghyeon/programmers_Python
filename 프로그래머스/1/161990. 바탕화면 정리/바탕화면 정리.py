b1416912d583eb51289393f8ca8e846a8a4f827f
최정현 <83526938+checkjunghyeon@users.noreply.github.com>
2024-11-01 17:43:18 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.54 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -1,19 +1,10 @@
 def solution(wallpaper):
-    files = [
-        [
-        wallpaper[i][j] 
-        for j in range(len(wallpaper[0]))
-        ]
-        for i in range(len(wallpaper))
-    ]
-
-    all_idx = [
-        (i,j) for i in range(len(files)) 
-        for j in range(len(files[0])) 
-        if files[i][j]=='#']
-    
-    x_idx = [x[0] for x in all_idx]
-    y_idx = [x[1] for x in all_idx]
+    x_idx, y_idx = [], []
+    for i, file in enumerate(wallpaper):
+        for j, f in enumerate(file):
+            if f == '#':
+                x_idx.append(i)
+                y_idx.append(j)
 
     answer = [min(x_idx), min(y_idx), max(x_idx)+1, max(y_idx)+1]
     
