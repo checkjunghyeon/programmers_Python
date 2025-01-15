e2b8ebb89a6af8b669d9b5710aba6dec7eef22fb
최정현 <83526938+checkjunghyeon@users.noreply.github.com>
2025-01-15 21:20:56 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586#qna) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.08 ms
+메모리: 10.2 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 24일 00:02:41
+2025년 01월 15일 21:20:53
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,17 +1,22 @@
 import math
 def solution(progresses, speeds):
-    answer = []
-
-    daysToComplete = list(map(lambda x, y: math.ceil((100 - x)/y), progresses, speeds))
+    answer = []        
+    n = len(progresses)
+    # 각 기능마다의 배포 가능일()
+    days_left = [math.ceil((100 - progresses[i]) / speeds[i]) for i in range(n)]
+    print(days_left)
+    cnt = 0
+    max_day = days_left[0]
     
-    currentMaxDay = daysToComplete[0]
-    n = 0
-    for i in range(len(daysToComplete)):
-        n += 1
-        if i == len(daysToComplete)-1 or currentMaxDay < daysToComplete[i+1]:
-            answer.append(n)
-            n = 0
-            if i != len(daysToComplete) - 1:
-                currentMaxDay = daysToComplete[i+1]
-        
+    for i in range(n):
+        # 배포 가능일이 가장 늦은 배포일보다 빠르면(맨 앞 친구와 같이 배포 가능하면)
+        if days_left[i] <= max_day:
+            cnt += 1
+        # 배포 가능일이 기준 배포일보다 느리면(맨 앞 친구보다 늦게 배포해야 한다면)
+        else:
+            answer.append(cnt)
+            cnt = 1
+            max_day = days_left[i]
+    answer.append(cnt)
+
     return answer
