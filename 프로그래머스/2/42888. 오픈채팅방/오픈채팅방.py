39b72816b9c45143c6e7d280dfd3b31991251b72
최정현 <83526938+checkjunghyeon@users.noreply.github.com>
2025-01-22 21:12:28 +0900

    [level 2] Title: 오픈채팅방, Time: 242.36 ms, Memory: 53.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -0,0 +1,35 @@
+import re
+
+def solution(record):
+    logs = []
+    user = {}
+    
+    for r in record:
+        user_data = r.split(" ")
+        mode, uid = user_data[:2]
+        nickname = user_data[-1] if len(user_data) > 2 else None
+
+        if mode == "Enter":
+            logs.append(str(uid) + '님이 들어왔습니다.')
+            user[uid] = nickname
+        elif mode == "Leave":
+            logs.append(str(uid) + '님이 나갔습니다.')
+        else:
+            user[uid] = nickname
+
+    keys = user.keys()
+    values = user.values()
+    
+
+    # print(f"{logs=}")
+    answer = update_logs(logs, user)
+    # print(f"{answer=}")
+    
+    return answer
+
+def update_logs(logs, user):
+    updated_logs = [
+        log.replace(log.split('님')[0], user.get(log.split('님')[0], log.split('님')[0]))
+        for log in logs
+    ]
+    return updated_logs
