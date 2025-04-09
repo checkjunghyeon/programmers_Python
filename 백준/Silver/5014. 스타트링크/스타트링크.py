c1cbc0699496f00e1a6f936bb80760941edd2b42
JungHyeon Choi <83526938+checkjunghyeon@users.noreply.github.com>
2025-04-09 15:15:53 +0900

    [Silver I] Title: 스타트링크, Time: 468 ms, Memory: 42144 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.py
+++ 백준/Silver/5014. 스타트링크/스타트링크.py
@@ -5,18 +5,17 @@ F, S, G, U, D = map(int, input().split())
 def BFS(start, target):
     queue = deque()
     queue.append((start, 0))    # 시작 버튼 누른 횟수는 0번
-    visited = [False] * F    # idx: 0 ~ (F-1)
-    dy = [U, -D]    # 위로 U층만큼, 아래로 D층만큼
+    visited = [False] * (F+1)   # idx 0은 사용 X
     
     while queue:
         current, count = queue.popleft()
         if current == target:
             return count
         
-        for i in range(2):
-            _next = current + dy[i]
-            if 1 <= _next < F+1 and not visited[_next-1]:
-                visited[_next-1] = True
+        for i in [U, -D]:
+            _next = current + i
+            if 1 <= _next < F+1 and not visited[_next]:
+                visited[_next] = True
                 queue.append((_next, count+1))
     return -1
 
