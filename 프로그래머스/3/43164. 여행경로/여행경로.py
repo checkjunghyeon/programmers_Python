5ac700823b16c6dd773b81dda4c2eedc51e22e7b
JungHyeon Choi <83526938+checkjunghyeon@users.noreply.github.com>
2025-08-10 08:11:11 +0900

    [level 3] Title: 여행경로, Time: 0.02 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,23 +1,22 @@
-from collections import deque
+from collections import defaultdict
 
 def dfs(start, graph, tickets):
     stack = []
     stack.append(start)
+    
     visited = []
     
     while stack:
-        cur = stack[-1]
-        if graph.get(cur, []):
-            stack.append(graph[cur].pop())
+        cur = stack[-1] # stack의 맨 위(top)
+        if graph[cur]:  # 현재 노드의 인접 노드가 존재한다면 
+            stack.append(graph[cur].pop()) # stack(도착지가 하나밖에 없어서 단순 append)
         else:
             visited.append(stack.pop())
     return visited[::-1]
 
 def solution(tickets):
-    graph = {}
+    graph = defaultdict(list)
     for a, b in tickets:
-        if a not in graph:
-            graph[a] = []
         graph[a].append(b)
     
     for a in graph:
