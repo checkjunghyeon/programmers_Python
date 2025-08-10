450f3cb3e3d6581fbeaec0ecba893c693e2c77bb
JungHyeon Choi <83526938+checkjunghyeon@users.noreply.github.com>
2025-08-10 12:48:10 +0900

    [level 3] Title: 정수 삼각형, Time: 55.99 ms, Memory: 17.9 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,8 +1,16 @@
 def solution(triangle):
-    level = len(triangle) # 5
-       
-    for i in range(level-2, -1, -1): # 3, 2, 1, 0
-        for j in range(i, -1, -1): # 4-0 / 3-0 / ... / 0
-            triangle[i][j] += max(triangle[i+1][j], triangle[i+1][j+1])
-            
-    return triangle[0][0]
+    level = len(triangle)
+    # DP 배열 초기화(2차원 배열)
+    F = [[0] * level for _ in range(level)]
+    
+    # DP 초기값 정의
+    F[0][0] = triangle[0][0]
+    
+    # DP식 반복
+    for i in range(1, level):
+        for j in range(0, i+1):
+            left = F[i-1][j] + triangle[i][j]
+            right = F[i-1][j-1] + triangle[i][j]
+            F[i][j] = max(left, right)
+    
+    return max(F[-1])
