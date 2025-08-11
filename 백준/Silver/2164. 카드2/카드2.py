8edb36e45d22a55111c1a7a8440e6b48ac3acbd3
JungHyeon Choi <83526938+checkjunghyeon@users.noreply.github.com>
2025-08-11 11:12:16 +0900

    [Silver IV] Title: 카드2, Time: 172 ms, Memory: 55756 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -3,9 +3,7 @@ from collections import deque
 N = int(input())
 
 # 카드 순서대로 넣기
-queue = deque()
-for i in range(N):
-    queue.append(i+1)
+queue = deque(list(range(1, N+1)))
 
 # 카드 버리는 과정 반복
 while len(queue) > 1:
