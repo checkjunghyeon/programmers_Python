a3bd9285024254b93686cc7cf1051d93c68cf828
JungHyeon Choi <83526938+checkjunghyeon@users.noreply.github.com>
2025-08-12 13:40:53 +0900

    [level 2] Title: 문자열 압축, Time: 3.40 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -1,24 +1,27 @@
 def solution(s):
-    result = []
-    if len(s) == 1:
+    n = len(s)
+    if n == 1:
         return 1
     
-    for i in range(1, len(s)+1):
-        comp = ""
-        pre = s[:i]
+    
+    ans = n
+    for k in range(1, n//2 + 1):
+        pre = s[:k] # [0,k] 구간
         cnt = 1
+        result = []
         
-        for j in range(i, len(s)+i, i):              
-            if pre == s[j:i+j]:
-                cnt += 1
-            else:
-                if cnt != 1:
-                    comp += (str(cnt) + pre)
-                else:
-                    comp += pre
-                    
-                pre = s[j:i+j]
-                cnt = 1
-        result.append(len(comp))
+        # 초반 k까지를 제외한 나머지 구간, k만큼 step
+        for i in range(k, n, k):
+            cur = s[i:i+k]  # k만큼 자른 문자열 구간
+            if cur == pre:  # 이전과 비교해서 중복되면
+                cnt += 1    # 카운팅
+            else:           # 이전과 중복되지 않는다면
+                result.append((str(cnt) if cnt > 1 else "") + pre)  # 1이상인 경우에 앞에 카운팅 숫자 추가
+                pre = cur   # 이전(비교대상) 갱신
+                cnt = 1     # 카운트 초기화
         
-    return min(result)
+        # 마지막 남은 이전(비교대상) 추가
+        result.append((str(cnt) if cnt > 1 else "") + pre)
+        ans = min(ans, len("".join(result)))
+        
+    return ans
